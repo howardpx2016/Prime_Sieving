a4d204b6a417f77223ca71e2b1b8ef33feb116e7
root <root@DESKTOP-10J9EA0.localdomain>
2021-03-13 04:46:38 +0800

    final tweaks, use of math functions, update makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC = g++
 CXXFLAGS = -O3 -mcx16 -march=native -std=c++17 -Wall -Wextra -Werror -fcilkplus -DCILK -fpic
 
-all: sieve-E-parallel2
+all: sieve-E-parallel
 
-reduce: sieve-E-parallel2.cpp get_time.h
+reduce: sieve-E-parallel.cpp get_time.h
 	$(CC) $(CXXFLAGS) reduce.cc -o reduce
 
 clean:
--- sieve-E-parallel.cpp
+++ sieve-E-parallel.cpp
@@ -3,27 +3,27 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <ctime>
+#include <cmath>
 #include "get_time.h"
 using namespace std;
  
-void SieveOfEratosthenes(int n) {
+void sieveE(int n) {
     bool *prime = new bool[n + 1];
     memset(prime, true, n+1); //set all values to true
  
-    for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true)
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (prime[i] == true)
         {
-            cilk_for (int i = p * p; i <= n; i += p)
-                prime[i] = false;
+            cilk_for (int p = pow(i,2); p <= n; p = p + i)
+                prime[p] = false;
         }
     }
- 
-    // Print all prime numbers
+    
+    //print prime count
     int pCount = 0;
-    for (int p = 2; p <= n; p++) { 
-        if (prime[p]) {
+    for (int j = 2; j <= n; j++) { 
+        if (prime[j] == true) {
             pCount++;
-            // cout << p << " ";
         }
     }
     cout << pCount;
@@ -33,14 +33,15 @@ void SieveOfEratosthenes(int n) {
  
 // Driver Code
 int main() {
-int testValue = 0;
+    int testValue = 0;
 
     for (int i = 0; i <= 10; i++) {
         testValue = pow(2, 20+i);
-        cout << "2^" << 20+i << ": ";
+        // cout << "2^" << 20+i << ": ";
+        cout << testValue << ": ";
         for (int i = 0; i < 3; i++) {
             timer t;
-            SieveOfEratosthenes(testValue);
+            sieveE(testValue);
             t.stop();
             cout << " - " << t.get_total() << ", ";
         }
